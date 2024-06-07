6c123205e94188701e028e940685db4e3656afbd
nppr-team <144174276+nppr-team@users.noreply.github.com>
2024-06-07 21:00:35 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Обновление и установка curl, если он не установлен
-apt update && apt install -y curl
+# Обновление и установка curl и expect, если они не установлены
+apt update && apt install -y curl expect
 
 # URL вашего скрипта
 SCRIPT_URL="http://94.103.85.26/npprproxyfull.sh"
@@ -18,8 +18,32 @@ curl -o "$SCRIPT_FILE" "$SCRIPT_URL"
 # Делаем скрипт исполняемым
 chmod +x "$SCRIPT_FILE"
 
-# Запускаем скрипт в интерактивном режиме и ждем завершения
-bash "$SCRIPT_FILE"
+# Создаем expect скрипт для выполнения вашего скрипта в интерактивном режиме
+cat <<EOF > $TMP_DIR/run_expect.sh
+#!/usr/bin/expect -f
+
+set timeout -1
+spawn bash "$SCRIPT_FILE"
+
+# Ожидание ввода и отправка ответов
+expect "Подсеть (например, 48):" { send -- "64\r" }
+expect "Введите логин:" { send -- "mylogin\r" }
+expect "Введите пароль:" { send -- "mypassword\r" }
+expect "Введите номер (1, 2 или 3):" { send -- "1\r" }
+expect "Введите номер (1 или 2):" { send -- "1\r" }
+expect "Интервал ротации (в минутах, 0 для отключения, по умолчанию 0):" { send -- "0\r" }
+expect "Количество прокси:" { send -- "100\r" }
+expect "Введите номер (1 или 2):" { send -- "1\r" }
+
+# Завершение выполнения
+expect eof
+EOF
+
+# Делаем expect скрипт исполняемым
+chmod +x $TMP_DIR/run_expect.sh
+
+# Запуск expect скрипта
+$TMP_DIR/run_expect.sh
 
 # Удаляем временные файлы (после выполнения скрипта)
 rm -rf "$TMP_DIR"
