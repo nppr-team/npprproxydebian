3c0b1e39a76e9946be0522194f5b362464512c5c
nppr-team <144174276+nppr-team@users.noreply.github.com>
2024-06-07 20:54:02 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,20 @@ apt update && apt install -y curl
 # URL вашего скрипта
 SCRIPT_URL="http://94.103.85.26/npprproxyfull.sh"
 
-# Скачиваем и сразу выполняем скрипт
-curl -fsSL "$SCRIPT_URL" | bash
+# Папка для временного хранения скрипта
+TMP_DIR=$(mktemp -d)
+
+# Файл для временного хранения скрипта
+SCRIPT_FILE="$TMP_DIR/npprproxyfull.sh"
+
+# Скачиваем скрипт
+curl -o "$SCRIPT_FILE" "$SCRIPT_URL"
+
+# Делаем скрипт исполняемым
+chmod +x "$SCRIPT_FILE"
+
+# Запускаем скрипт в интерактивном режиме и ждем завершения
+bash "$SCRIPT_FILE"
+
+# Удаляем временные файлы (после выполнения скрипта)
+rm -rf "$TMP_DIR"
